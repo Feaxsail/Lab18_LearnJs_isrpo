c3bc272ae907d0fada07d34a08708319df4bfaec
whypikmi <ttelatnikova1@yandex.ru>
2026-03-13 09:59:13 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let userName = "Алексей";
 console.log(`Привет, ${userName}!`);
 
 let price = 99.99;
-let temperature = -15;
+let tempValue = -15;
 let infinity = 1 / 0;
 let notANumber = 0 / 0;
 console.log(0.1 + 0.2);
@@ -156,3 +156,45 @@ let a2 = 5;
 let b2 = 10;
 console.log(a2 + b2);
 console.log(a2 * b2);
+
+let temperature = 25;
+if (temperature < 0) {
+    console.log("Холодно");
+} else if (temperature >= 0 && temperature <= 20) {
+    console.log("Прохладно");
+} else {
+    console.log("Тепло");
+}
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченный доступ");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let a3 = 10;
+let b3 = "10";
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let monthNumber = 5;
+switch (monthNumber) {
+    case 1: console.log("Январь"); break;
+    case 2: console.log("Февраль"); break;
+    case 3: console.log("Март"); break;
+    case 4: console.log("Апрель"); break;
+    case 5: console.log("Май"); break;
+    case 6: console.log("Июнь"); break;
+    case 7: console.log("Июль"); break;
+    case 8: console.log("Август"); break;
+    case 9: console.log("Сентябрь"); break;
+    case 10: console.log("Октябрь"); break;
+    case 11: console.log("Ноябрь"); break;
+    case 12: console.log("Декабрь"); break;
+    default: console.log("Неверное значение");
+}
